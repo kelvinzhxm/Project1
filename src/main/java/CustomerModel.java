07951b7bd71ddf9001ac0e8acbc8fa55cbdfb12c
kelvinzhxm <54419668+kelvinzhxm@users.noreply.github.com>
2019-10-17 11:29:18 -0500

    src

---
+++ src/main/java/CustomerModel.java
@@ -0,0 +1,14 @@
+public class CustomerModel {
+    public int mCustomerID;
+    public String mPhone;
+    public String mName, mAddress, mPaymentINFO;
+    public String toString() {
+        StringBuilder sb = new StringBuilder("(");
+        sb.append(mCustomerID).append(",");
+        sb.append("\"").append(mName).append("\"").append(",");
+        sb.append("\"").append(mAddress).append("\"").append(",");
+        sb.append(mPhone).append(",");
+        sb.append("\"").append(mPaymentINFO).append("\"").append(")");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/SQLiteDataAdapter.java
@@ -0,0 +1,153 @@
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteDataAdapter implements IDataAdapter {
+
+
+    Connection conn = null;
+
+    public int connect(String dbfile) {
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:" + dbfile;
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return CONNECTION_OPEN_FAILED;
+        }
+        return CONNECTION_OPEN_OK;
+    }
+
+    @Override
+    public int disconnect() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return CONNECTION_CLOSE_FAILED;
+        }
+        return CONNECTION_CLOSE_OK;
+    }
+
+    public ProductModel loadProduct(int productID) {
+        ProductModel product = new ProductModel();
+
+        try {
+            String sql = "SELECT ProductID, Barcode, Name, Expiration, Date, Price, TaxRate, Quantity, Supplier, ManufacturedDate FROM Product WHERE ProductID = " + productID;
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            product.mProductID = rs.getInt("ProductId");
+            product.mBarcode = rs.getInt("Barcode");
+            product.mName = rs.getString("Name");
+            product.mExpiration = rs.getString("Expiration");
+            product.mDate = rs.getString("Date");
+            product.mPrice = rs.getDouble("Price");
+            product.mTaxRate = rs.getDouble("TaxRate");
+            product.mQuantity = rs.getInt("Quantity");
+            product.mSupplier = rs.getString("Supplier");
+            product.mManufacturedDate = rs.getString("ManufacturedDate");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return product;
+    }
+
+    public int saveProduct(ProductModel product) {
+        try {
+            String sql = "INSERT INTO Product(ProductID, Barcode, Name, Expiration, Date, Price, TaxRate, Quantity, Supplier, ManufacturedDate) VALUES " + product;
+            System.out.println(sql);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+
+        } catch (Exception e) {
+            String msg = e.getMessage();
+            System.out.println(msg);
+            if (msg.contains("UNIQUE constraint failed"))
+                return PRODUCT_DUPLICATE_ERROR;
+        }
+
+        return PRODUCT_SAVED_OK;
+    }
+
+    public CustomerModel loadCustomer(int customerID) {
+        CustomerModel customer = new CustomerModel();
+
+        try {
+            String sql = "SELECT CustomerID, Name, Address, Phone, PaymentINFO FROM Customer WHERE CustomerID = " + customerID;
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            customer.mCustomerID = rs.getInt("CustomerId");
+            customer.mName = rs.getString("Name");
+            customer.mAddress = rs.getString("Address");
+            customer.mPhone = rs.getString("Phone");
+            customer.mPaymentINFO = rs.getString("PaymentINFO");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return customer;
+    }
+
+    public int saveCustomer(CustomerModel customer) {
+        try {
+            String sql = "INSERT INTO Customer(CustomerID, Name, Address, Phone, PaymentInfo) VALUES " + customer;
+            System.out.println(sql);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            String msg = e.getMessage();
+            System.out.println(msg);
+            if (msg.contains("UNIQUE constraint failed"))
+                return CUSTOMER_DUPLICATE_ERROR;
+        }
+
+        return CUSTOMER_SAVED_OK;
+    }
+
+    public PurchaseModel loadPurchase(int purchaseID) {
+        PurchaseModel purchase = new PurchaseModel();
+
+        try {
+            String sql = "SELECT PurchaseID, CustomerID, ProductID, Datetime, Quantity, Price, Tax, TotalCost FROM Purchase WHERE PurchaseID = " + purchaseID;
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            purchase.mPurchaseID = rs.getInt("PurchaseID");
+            purchase.mCustomerID = rs.getInt("CustomerID");
+            purchase.mProductID = rs.getInt("ProductID");
+            purchase.mDate = rs.getString("Datetime");
+            purchase.mQuantity = rs.getInt("Quantity");
+            purchase.mPrice = rs.getDouble("Price");
+            purchase.mTax = rs.getDouble("Tax");
+            purchase.mTotal = rs.getDouble("TotalCost");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return purchase;
+    }
+
+    public int savePurchase(PurchaseModel purchase) {
+        try {
+            String sql = "INSERT INTO Purchase VALUES " + purchase;
+            System.out.println(sql);
+            TXTReceiptBuilder receipt = new TXTReceiptBuilder();
+            receipt.appendPurchase(purchase);
+            System.out.println("Receipt" + purchase);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            String msg = e.getMessage();
+            System.out.println(msg);
+            if (msg.contains("UNIQUE constraint failed"))
+                return PURCHASE_DUPLICATE_ERROR;
+        }
+        return PURCHASE_SAVED_OK;
+    }
+
+    }
